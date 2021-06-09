1ef8e18e0237829a353d0c7b9788b6dc3b8fb07e
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-06-09 12:09:26 +0200

    Update installeur.py

--- versions/installeur.py
+++ versions/installeur.py
@@ -1,12 +1,15 @@
 import sys, os
 from urllib.request import urlopen
 
+ip_path = os.path.dirname(sys.argv[0])+"/I-python"
+os.makedirs(ip_path)
+
 input("Appuyez sur entrée après avoir lu et accepté les conditions d’utilisation\n")
 done = 1
 
 print("Téléchargement du sous système cytron depuis le serveur de la passemblage")
 try:
-    with open(os.path.dirname(sys.argv[0])+"/cytron.py", 'wb') as img:
+    with open(ip_path+"/cytron.py", 'wb') as img:
         img.write(urlopen('https://raw.githubusercontent.com/passemblage/I-python-Public/main/versions/%5BQ/cytron.py').read())
         print("cytron.py installé avec succès\n")
 except:
@@ -16,7 +19,7 @@ except:
 
 print("Téléchargement du fichier principal depuis le serveur de la passemblage")
 try:
-    with open(os.path.dirname(sys.argv[0])+"/main.py", 'wb') as img:
+    with open(ip_path + "/main.py", 'wb') as img:
         img.write(urlopen('https://raw.githubusercontent.com/passemblage/I-python-Public/main/versions/%5BQ/main.py').read())
         print("main.py installé avec succès\n")
 except:
@@ -26,6 +29,9 @@ except:
 
 if done == 1:
     input("Tout les fichiers on était installés avec succès\n\nAppuyez sur entrée pour lancé I-python")
-    os.system("python "+os.path.dirname(sys.argv[0])+"/main.py")
+    try:
+        os.system("python "+ ip_path + "/main.py")
+    except:
+        input("échec: merci de lancé main.py manuellement")
 else:
-    input("échec appuyez sur entrée pour quitter")
+    input("échec: appuyez sur entrée pour quitter")
